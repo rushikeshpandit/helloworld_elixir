fa239a84e0a026b2f9c0d2e81ecb93a5e6ac11f6
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-11-01 16:55:34 +0530

    - Module plug example

--- lib/hello_world_web/router.ex
+++ lib/hello_world_web/router.ex
@@ -8,6 +8,7 @@ defmodule HelloWorldWeb.Router do
     plug :put_root_layout, html: {HelloWorldWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug HelloWorldWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
